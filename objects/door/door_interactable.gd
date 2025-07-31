a0b7de4e1d1eb1fd415245e59ff293211a26bba3
Flynn Mattson <flynnmattson@gmail.com>
2025-07-30 21:54:53 -0700

    close door

--- objects/door/door_interactable.gd
+++ objects/door/door_interactable.gd
@@ -1,6 +1,17 @@
-extends Interactable
+class_name DoorInteractable extends Interactable
 
 @export var animation_player: AnimationPlayer
 
+var is_locked: bool = false
+var is_open: bool = false
+
+func _ready() -> void:
+	pass
+
 func interact() -> void:
-	animation_player.play("open")
+	if not is_locked and not animation_player.is_playing():
+		if is_open:
+			animation_player.play("close")
+		else:
+			animation_player.play("open")
+		is_open = !is_open
