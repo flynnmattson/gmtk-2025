4a4e3d25fd51be6135175d65ab6b37c57b19918b
Flynn Mattson <flynnmattson@gmail.com>
2025-07-30 23:11:24 -0700

    correct ball physics when being held

--- controller/interactive_controller.gd
+++ controller/interactive_controller.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var raycast : RayCast3D
 @export var hold_location: Node3D
+@export var throw_power: int = 10
 
 var active_throwable: Throwable
 
@@ -16,7 +17,7 @@ func _process(_delta: float) -> void:
 				active_throwable = collider as Throwable
 				active_throwable.get_parent().remove_child(active_throwable)
 				hold_location.add_child(active_throwable)
-				active_throwable.freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
+				active_throwable.hold()
 				active_throwable.global_position = hold_location.global_position
 
 	if Input.is_action_just_pressed("attack") and active_throwable:
@@ -24,8 +25,8 @@ func _process(_delta: float) -> void:
 		hold_location.remove_child(active_throwable)
 		world_objects.add_child(active_throwable)
 		active_throwable.global_position = hold_location.global_position
-		active_throwable.freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+		active_throwable.let_go()
 		var direction = raycast.global_position.direction_to(raycast.to_global(raycast.target_position))
 		direction.y += randf_range(0.25, 0.5)
-		active_throwable.apply_central_impulse(direction * 10)
+		active_throwable.apply_central_impulse(direction * throw_power)
 		active_throwable = null
